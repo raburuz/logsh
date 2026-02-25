1e86e112c9a8d38c2eed4e23f80c941eef4b6245
Jean Pablo <jeanpablocalderonramirez@gmail.com>
2026-02-25 15:47:54 +0100

    drizzle options

--- modules/db/drizzle.config.ts
+++ modules/db/drizzle.config.ts
@@ -7,7 +7,7 @@ const config = defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL || '',
-    ssl: isProduction ? 'verify-full' : false
+    //ssl: isProduction ? 'verify-full' : false
   },
   strict: true, // Enable strict mode for better type safety
   verbose: true, // Enable verbose logging for better insights during development
